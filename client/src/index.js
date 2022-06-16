10708f40587c8c5ebe45870bd49069c1f1a21a95
Vũ Duy <ducduy23899@gmail.com>
2022-06-16 09:33:03 +0700

    add styles app2

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
-
+import "./styles/app.css";
+import "./styles/app2.css";
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
