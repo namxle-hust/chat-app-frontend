ce4e7576984a2fba19d195723b1ee2d0b4f8ace2
hoangrbs <hoangrbs@gmail.com>
2022-06-26 01:20:51 +0700

    edit Share component

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,102 +1,88 @@
 // do not delete, later to perform file upload
+import './Share.css';
 
 import {
   PermMedia,
-  Label,
-  Room,
-  EmojiEmotions,
   Cancel,
 } from "@material-ui/icons";
-import { useContext, useRef, useState } from "react";
-import { AuthContext } from "../../context/AuthContext";
+import { useState } from "react";
 import axios from "axios";
+import { axiosHeadersObject } from '../../utils-contants';
 
-export default function Share() {
-  const { user } = useContext(AuthContext);
+export default function Share({disableTextInput, setDisableTextInput, setNewMessage, handleSubmit}) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  const desc = useRef();
+
   const [file, setFile] = useState(null);
 
+  
   const submitHandler = async (e) => {
     e.preventDefault();
-    const newPost = {
-      userId: user.id,
-      desc: desc.current.value,
-    };
+    
     if (file) {
       const data = new FormData();
       const fileName = Date.now() + file.name;
       data.append("name", fileName);
-      data.append("file", file);
-      newPost.img = fileName;
-      console.log(newPost);
+      data.append("image", file);
+      
+      // console.log('upload file: ', file);
+      // console.log('upload data: ', data);
+
       try {
-        await axios.post("/upload", data);
-      } catch (err) {}
+        const res = await axios.post("/upload/image", data, axiosHeadersObject());
+        // console.log('res: ', res);
+        const imageUrl = res.data.data.file_path.fd;
+        setNewMessage({ type: 'image', url: imageUrl });
+        
+        setFile(null);
+        setDisableTextInput(false);
+      } catch (err) {
+        console.log(err);
+      }
     }
-    try {
-      await axios.post("/posts", newPost);
-      window.location.reload();
-    } catch (err) {}
+    
   };
 
   return (
-    <div className="share">
-      <div className="shareWrapper">
-        <div className="shareTop">
-          <img
-            className="shareProfileImg"
-            src={
-              user.profilePicture
-                ? PF + user.profilePicture
-                : PF + "person/noAvatar.png"
-            }
-            alt=""
-          />
-          <input
-            placeholder={"What's in your mind " + user.username + "?"}
-            className="shareInput"
-            ref={desc}
-          />
-        </div>
-        <hr className="shareHr" />
+    
+    <div className="share btn-light" >
         {file && (
           <div className="shareImgContainer">
             <img className="shareImg" src={URL.createObjectURL(file)} alt="" />
-            <Cancel className="shareCancelImg" onClick={() => setFile(null)} />
+            <Cancel className="shareCancelImg" onClick={() => {
+              setDisableTextInput(false);
+              setFile(null);
+            }} />
           </div>
         )}
-        <form className="shareBottom" onSubmit={submitHandler}>
+
+        <div className="shareBottom">
           <div className="shareOptions">
             <label htmlFor="file" className="shareOption">
               <PermMedia htmlColor="tomato" className="shareIcon" />
-              <span className="shareOptionText">Photo or Video</span>
+              <span className="shareOptionText"> 
+              </span>
               <input
                 style={{ display: "none" }}
                 type="file"
                 id="file"
                 accept=".png,.jpeg,.jpg"
-                onChange={(e) => setFile(e.target.files[0])}
+                onChange={(e) => {
+                  setDisableTextInput(true);
+                  setFile(e.target.files[0]);
+                }}
               />
             </label>
-            <div className="shareOption">
-              <Label htmlColor="blue" className="shareIcon" />
-              <span className="shareOptionText">Tag</span>
-            </div>
-            <div className="shareOption">
-              <Room htmlColor="green" className="shareIcon" />
-              <span className="shareOptionText">Location</span>
-            </div>
-            <div className="shareOption">
-              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
-              <span className="shareOptionText">Feelings</span>
-            </div>
           </div>
-          <button className="shareButton" type="submit">
-            Share
-          </button>
-        </form>
-      </div>
+          <div>
+          {
+            !disableTextInput? <></> :
+            <button className ="btn btn-primary shareButton" type='submit' onClick={submitHandler}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className ="feather feather-send"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
+            </button>
+          }
+          </div>
+        </div>
+
     </div>
   );
 }
