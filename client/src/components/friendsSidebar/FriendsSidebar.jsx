d6f86b6db5c9e690bd023ea56f2c8d3381b6deb7
hoangrbs <hoangrbs@gmail.com>
2022-06-26 01:20:07 +0700

    friendsSidebar Message Navigation ProfileBar

--- client/src/components/friendsSidebar/FriendsSidebar.jsx
+++ client/src/components/friendsSidebar/FriendsSidebar.jsx
@@ -8,8 +8,9 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
  // ------------- ./dropdown menu import components ------------------ 
 
-export default function FriendsSidebar ({ onlineUsersId, currentId, setCurrentChat, setProfileBarActive }) {
+export default function FriendsSidebar ({ onlineUsersId, currentId, setCurrentChat, setProfileBarActive, setProfileBarUserInfo }) {
     const [allFriends, setAllFriends] = useState([]);
+    const [clickedUser, setClickedUser] = useState(null);
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
     useEffect(() => {
@@ -55,100 +56,112 @@ export default function FriendsSidebar ({ onlineUsersId, currentId, setCurrentCh
     // ------------- </ drop down menu function> -------------
 
     return (
-        <div id="friends" class="sidebar active">
+        <div id="friends" className ="sidebar active">
             <header>
                 <span>Friends</span>
-                {/* <ul class="list-inline">
-                    <li class="list-inline-item" data-toggle="tooltip" title="" data-original-title="Add friends">
-                        <a class="btn btn-outline-light" href="#" data-toggle="modal" data-target="#addFriends">
+                {/* <ul className ="list-inline">
+                    <li className ="list-inline-item" data-toggle="tooltip" title="" data-original-title="Add friends">
+                        <a className ="btn btn-outline-light" href="#" data-toggle="modal" data-target="#addFriends">
                             <svg xmlns="http://www.w3.org/2000/s
-                            vg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
+                            vg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className ="feather feather-user-plus"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
                         </a>
                     </li>
-                    <li class="list-inline-item d-xl-none d-inline">
-                        <a href="#" class="btn btn-outline-light text-danger sidebar-close">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                    <li className ="list-inline-item d-xl-none d-inline">
+                        <a href="#" className ="btn btn-outline-light text-danger sidebar-close">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className ="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                         </a>
                     </li>
                 </ul> */}
             </header>
             <form>
-                <input type="text" class="form-control" placeholder="Search friends" />
+                <input type="text" className ="form-control" placeholder="Search friends" />
             </form>
-            <div class="sidebar-body" tabindex="3" style = {{ overFlow: 'hidden', outline: 'none' }}>
-                <ul class="list-group list-group-flush">
-                    {allFriends.map((o) => (
-                        <li class="list-group-item" data-navigation-target="chats" key={o.id}>
-                            {
-                                onlineUsersId.includes(o.id) ? 
-                                <div onClick={() => { handleClick(o)}}>
-                                    <figure class="avatar avatar-state-success">
-                                        <img 
-                                            src={
-                                                o?.profile_pic_url
-                                                ? o.profile_pic_url
-                                                : PF + "person/noAvatar.png"
-                                            } 
-                                            class="rounded-circle" 
-                                            alt="image" 
-                                        />
-                                    </figure>
-                                </div>
-                                : 
-                                <div onClick={() => { handleClick(o)}}>
-                                    <figure class="avatar">
-                                        <img 
-                                            src={
-                                                o?.profile_pic_url
-                                                ? o.profile_pic_url
-                                                : PF + "person/noAvatar.png"
-                                            } 
-                                            class="rounded-circle" 
-                                            alt="image" 
-                                        />
-                                    </figure>
-                                </div>
-                            }
-                            <div class="users-list-body">
-                                <div onClick={() => { handleClick(o)}}>
-                                    <h5> {o?.user_name} </h5>
-                                </div>
-                                <div 
-                                    class="users-list-action"
-                                    id="basic-button"
-                                    aria-controls={open ? 'basic-menu' : undefined}
-                                    aria-haspopup="true"
-                                    aria-expanded={open ? 'true' : undefined}
-                                    onClick={handleOpenMenu}
-                                >    
-                                    <div class="action-toggle">   
-                                        <div class="dropdown">
-                                            <a href="#">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                            </a>
+            <div className ="sidebar-body" tabIndex="3" style = {{ overFlow: 'hidden', outline: 'none' }}>
+                <ul className ="list-group list-group-flush">
+                    {allFriends.map((o) => {
+
+                        return (
+                            <li className ="list-group-item" data-navigation-target="chats" key={o.id}>
+                                {
+                                    onlineUsersId.includes(o.id) ? 
+                                    <div onClick={() => { handleClick(o)}}>
+                                        <figure className ="avatar avatar-state-success">
+                                            <img 
+                                                src={
+                                                    o?.profile_pic_url
+                                                    ? o.profile_pic_url
+                                                    : PF + "person/noAvatar.png"
+                                                } 
+                                                className ="rounded-circle" 
+                                                alt="image" 
+                                            />
+                                        </figure>
+                                    </div>
+                                    : 
+                                    <div onClick={() => { handleClick(o) }}>
+                                        <figure className ="avatar">
+                                            <img 
+                                                src={
+                                                    o?.profile_pic_url
+                                                    ? o.profile_pic_url
+                                                    : PF + "person/noAvatar.png"
+                                                } 
+                                                className ="rounded-circle" 
+                                                alt="image" 
+                                            />
+                                        </figure>
+                                    </div>
+                                }
+                                <div className ="users-list-body">
+                                    <div onClick={() => {
+                                        handleClick(o);
+                                    }}>
+                                        <h5> {o?.user_name} </h5>
+                                    </div>
+                                    <div 
+                                        className ="users-list-action"
+                                        id="basic-button"
+                                        aria-controls={open ? 'basic-menu' : undefined}
+                                        aria-haspopup="true"
+                                        aria-expanded={open ? 'true' : undefined}
+                                        onClick={(e) => {
+                                            handleOpenMenu(e);
+                                            setClickedUser(o);
+                                        }}
+                                    >    
+                                        <div className ="action-toggle">   
+                                            <div className ="dropdown">
+                                                <a href="#">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className ="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
+                                    <Menu
+                                        id="basic-menu"
+                                        anchorEl={anchorEl}
+                                        open={open}
+                                        onClose={handleClose}
+                                        MenuListProps={{
+                                            'aria-labelledby': 'basic-button',
+                                        }}
+                                    >
+                                        <MenuItem onClick={() => {
+                                            handleClose();
+                                            setProfileBarActive(true); 
+                                            setProfileBarUserInfo(clickedUser);
+                                        }}> 
+                                            Profile
+                                        </MenuItem>
+                                    </Menu>
                                 </div>
-                                <Menu
-                                    id="basic-menu"
-                                    anchorEl={anchorEl}
-                                    open={open}
-                                    onClose={handleClose}
-                                    MenuListProps={{
-                                        'aria-labelledby': 'basic-button',
-                                    }}
-                                >
-                                    <MenuItem onClick={() => {
-                                        handleClose();
-                                        setProfileBarActive(true); 
-                                    }}>Profile</MenuItem>
-                                </Menu>
-                            </div>
-                        </li>
-                    ))}
-
+                            </li>
+                        ) 
+                    })}
                 </ul>
             </div>
         </div>
     );
 }
+
+
--- client/src/components/message/Message.jsx
+++ client/src/components/message/Message.jsx
@@ -1,23 +1,38 @@
 
 import { format } from "timeago.js";
+import { CheckCircle, CheckCircleOutline } from '@material-ui/icons';
 
-export default function Message({ message, messageType, own, senderUsername, senderProfilePicture, messageTime }) {
+export default function Message({ message, messageType, own, senderUsername, senderProfilePicture, messageTime, isLastMessSent, isLastMessDelivered }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
   return (
-    <div class= { own ? 'message-item outgoing-message' : 'message-item'}>
-      <div class="message-avatar">
-          <figure class="avatar">
-              <img src={ senderProfilePicture ? senderProfilePicture : PF + "person/noAvatar.png"} class="rounded-circle" alt="image" />
-          </figure>
-          <div>
-              <h5>{senderUsername}</h5>
-              <div class="time"> {format(messageTime)} </div>
-          </div>
+    <>
+      <div className = { own ? 'message-item outgoing-message' : 'message-item'}>
+        <div className ="message-avatar">
+            <figure className ="avatar">
+                <img src={ senderProfilePicture ? senderProfilePicture : PF + "person/noAvatar.png"} className ="rounded-circle" alt="image" />
+            </figure>
+            <div>
+                <h5>{senderUsername}</h5>
+                <div className ="time"> {format(messageTime)} </div>
+            </div>
+        </div>
+        <div className ="message-content">
+          {messageType !== 'image_url' ? 
+            message : 
+            <img src={message} 
+                 style = {{
+                  height: '250px',
+                  width: '100%',
+                  objectFit: 'cover'
+                 }} 
+            />}
+        </div>
+        {
+            isLastMessSent && isLastMessDelivered ? <CheckCircle fontSize="small" color='primary' /> : 
+            isLastMessSent ? <CheckCircleOutline fontSize="small" /> : <></>
+        }
       </div>
-      <div class="message-content">
-        {message}
-      </div>
-    </div>
+    </>
   );
 }
