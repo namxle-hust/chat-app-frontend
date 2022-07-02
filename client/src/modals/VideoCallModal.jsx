e3017f109236dacec6b8e889c9b2b73eb5009de1
hoangrbs <hoangrbs@gmail.com>
2022-07-02 17:55:15 +0700

    fix video call modal

--- client/src/modals/VideoCallModal.jsx
+++ client/src/modals/VideoCallModal.jsx
@@ -18,14 +18,14 @@ export default function VideoCallModal({
   arrivalMessage
 }) {
 
-  useEffect(() => {
+  // useEffect(() => {
     
-    if (arrivalMessage?.message_type === 'call' && arrivalMessage?.message === 'Missed Call') {
-      handleClose();
-      setCurrentCallingUser(null);
-      setCurrentCallId(null);
-    }
-  }, [arrivalMessage]);
+  //   if (arrivalMessage?.message_type === 'call' && arrivalMessage?.message === 'Missed Call') {
+  //     handleClose();
+  //     setCurrentCallingUser(null);
+  //     setCurrentCallId(null);
+  //   }
+  // }, [arrivalMessage]);
   
   const handleVideoCall =() => {
     function createPopupWin(pageURL, pageTitle, width, height) {
@@ -73,7 +73,10 @@ export default function VideoCallModal({
       >
         <div class="modal call fade show" id="videoCall" tabIndex="-1" role="dialog" aria-modal="true" style={{ display: 'block'}}>
             <div class="modal-dialog modal-dialog-centered modal-dialog-zoom" role="document">
-                <div class="modal-content">
+                <div class="modal-content" style = {{
+                  width: '450px',
+                  height: '375px'
+                }}>
                     <div class="modal-body">
                         <div class="call">
                             <div>
@@ -99,6 +102,10 @@ export default function VideoCallModal({
                                 <h4 style = {{ marginTop: '40px' }}> {currentCallingUser.user_name} <span class="text-success">video calling...</span></h4>
                                 <div class="action-button">
                                     <button type="button" class="btn btn-danger btn-floating btn-lg" data-dismiss="modal" 
+                                      style = {{
+                                        width: '55px',
+                                        height: '55px'
+                                      }}
                                       onClick = {(e) => {
                                         e.preventDefault();
                                         handleClose();
@@ -110,6 +117,10 @@ export default function VideoCallModal({
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                                     </button>
                                     <button type="button" class="btn btn-success btn-pulse btn-floating btn-lg" 
+                                          style = {{
+                                            width: '55px',
+                                            height: '55px'
+                                          }}
                                           onClick = {(e) => {
                                             e.preventDefault();
                                             handleClose();
