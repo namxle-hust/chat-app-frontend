fb7cedc334087d8a7f4f15951bd71b4546524ed8
Vu Duy <ducduy23899@gmail.com>
2022-07-06 09:27:23 +0700

    emoji

--- client/src/components/emojiPicker/EmojiPicker.jsx
+++ client/src/components/emojiPicker/EmojiPicker.jsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import { useState } from 'react';
-import Picker from 'emoji-picker-react';
+import * as React from "react";
+import { useState } from "react";
+import Picker from "emoji-picker-react";
 
-export default function EmojiPicker ({ newMessage, setNewMessage }) {
+export default function EmojiPicker({ newMessage, setNewMessage }) {
   // const [chosenEmoji, setChosenEmoji] = useState(null);
-
 
   const onEmojiClick = (event, emojiObject) => {
     // console.log(emojiObject);
@@ -13,17 +12,31 @@ export default function EmojiPicker ({ newMessage, setNewMessage }) {
   };
 
   return (
-    <div style = {{
-        position: 'absolute',
-        left: '10px',
-        top: '-310px'
-    }}>
+    <div
+      style={{
+        position: "absolute",
+        left: "10px",
+        top: "-310px",
+      }}
+    >
       {/* {chosenEmoji ? (
         <span>You chose: {chosenEmoji.emoji}</span>
       ) : (
         <span>No emoji Chosen</span>
       )} */}
-      <Picker onEmojiClick={onEmojiClick} />
+      <Picker
+        onEmojiClick={onEmojiClick}
+        groupVisibility={{
+          flags: false,
+          symbols: false,
+          objects: false,
+          activities: false,
+          travel_places: false,
+          recently_used: false,
+          food_drink: false,
+          animals_nature: false,
+        }}
+      />
     </div>
   );
-};
+}
